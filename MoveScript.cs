3d58ee7bd391c98dfb7cd70f059e98e5d0bc9ab6
yvnix <101469862+yvnix@users.noreply.github.com>
2023-02-03 11:45:24 +0100

    Script added to GitHub.
    I added moving, jumping and looking around.
    Walking As usual, it is classically set to the so-called. WASD.

---
+++ MoveScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private float rotationSpeed = 90.0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        // jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+
+        // looking around
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
